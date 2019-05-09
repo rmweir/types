c420388fd256f1c7e8937557e5fed48b5387f8c2
rmweir <ricardo.weir1@gmail.com>
2019-05-09 09:46:10 -0700

    example type

---
+++ apis/management.cattle.io/v3/example_type.go
@@ -0,0 +1,41 @@
+package v3
+
+import (
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type ExampleConfig struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object’s metadata. More info:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#metadata
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	// Specification of the desired behavior of the the cluster. More info:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#spec-and-status
+	Spec   ExampleSpec   `json:"spec,omitempty"`
+	Status ExampleStatus `json:"status,omitempty"`
+}
+
+type ExampleSpec struct {
+	ExampleOne string `json:"exampleOne,omitempty"`
+	ExampleTwo int `json:"exampleOne,omitempty"`
+}
+
+type ExampleStatus struct {
+	Conditions []ExampleCondition `json:"conditions,omitempty"`
+}
+
+type ExampleCondition struct {
+	// Type of cluster condition.
+	Type string `json:"type"`
+	// Status of the condition, one of True, False, Unknown.
+	Status v1.ConditionStatus `json:"status"`
+	// The last time this condition was updated.
+	LastUpdateTime string `json:"lastUpdateTime,omitempty"`
+	// Last time the condition transitioned from one status to another.
+	LastTransitionTime string `json:"lastTransitionTime,omitempty"`
+	// The reason for the condition's last transition.
+	Reason string `json:"reason,omitempty"`
+	// Human-readable message indicating details about last transition
+	Message string `json:"message,omitempty"`
+}
