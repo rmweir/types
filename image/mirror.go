bbd20f0184fcb66449ada732539cd6d324dfde84
rmweir <ricardo.weir1@gmail.com>
2020-03-19 17:50:37 -0700

    Stop mirroring coredns image

--- image/mirror.go
+++ image/mirror.go
@@ -22,7 +22,6 @@ func Mirror(image string) string {
 	image = strings.Replace(image, "alpine/git", "rancher/alpine-git", 1)
 	image = strings.Replace(image, "prom/", "rancher/prom-", 1)
 	image = strings.Replace(image, "quay.io/pires", "rancher", 1)
-	image = strings.Replace(image, "coredns/", "rancher/coredns-", 1)
 	image = strings.Replace(image, "minio/", "rancher/minio-", 1)
 
 	Mirrors[image] = orig
